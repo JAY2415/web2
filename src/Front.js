78e1526010e99c757c04b0c86566ad590a929fe1
JAY2415 <137811465+JAY2415@users.noreply.github.com>
2023-12-24 12:09:58 +0530

    Update Front.js

--- src/Front.js
+++ src/Front.js
@@ -5,6 +5,13 @@ function Front() {
   <div>
     <img src={logo} width="40px" />
     <h1>Fun Facts About React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on GitHub</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including movile</li>
+    </ul>
   </div>
 );
 }
